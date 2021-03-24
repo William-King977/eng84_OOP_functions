73fd1478427c26cf6ff0a3c17eca26b959f2cbfc
William King <williamking9772@gmail.com>
2021-03-24 16:12:47 +0000

    added functions section

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# Object Orientated Programming and Functions
+# Object Oriented Programming and Functions
 ## Functions
-### DRY
+A function is a block of code that only runs when it is 
+called, and it carries out a single purpose. Functions
+encourage the DRY (Don't Repeat Yourself) principle 
+by reducing repetition and clutter in code.
+
+A standard function
+```python
+# Calling the function will output the message.
+def greeting():
+    print("Welcome on board.")
+
+# The function must be called to display the output
+greeting()
+```
+
+A function taking in arguments
+```python
+def greeting(name):
+    print("Welcome on board " + name + ".")
+
+# Outputs "Welcome on board King."
+greeting("King")
+```
+
+A function returning a value
+```python
+def sum_return(num1, num2):
+    return num1 + num2
+
+# Outputs 6
+print(sum_return(2, 4))
+```
+
+### Use cases
+* Allow sections of code to be reusable
+* Reduces repetition of code
+* Easier to find bugs
+* When different arguments are needed for the same operations
+
+### Best practices
+* A function should carry out a single purpose
+* Should be named based on its purpose
--- functions.py
+++ functions.py
@@ -1,2 +1,68 @@
 # What are functions?
 # Why functions?
+
+# Syntax def function_name():
+# def tells the interpreter that the function is being created
+
+# Creating a greeting function
+# def greeting():
+#     print("Welcome on board.")
+#
+# # The function must be called to display the output
+# greeting()
+
+# Function to take an argument
+# def greeting(name):
+#     print("Welcome on board " + name + ".")
+# greeting("King")
+
+# Two arguments. Outputs the addition of two numbers.
+# def add(num1, num2):
+#     print(num1 + num2)
+# add(4, 10)
+
+# Using a return statement
+# def sum_return(num1, num2):
+#     return num1 + num2
+# print(sum_return(2, 4))
+
+# Create a calculator to add, subtract, divide, multiply
+# display appropriate messages with the computation values as to what the outcome is from each function
+# create a greeting function by taking user input as the user name and display it with the greeting message
+
+# Addition
+def add(num1, num2):
+    return num1 + num2
+
+# Subtraction
+def subtract(num1, num2):
+    return num1 - num2
+
+# Division
+def divide(num1, num2):
+    return num1 / num2
+
+# Multiplication
+def multiply(num1, num2):
+    return num1 * num2
+
+# Enter two numbers then calculate them
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(f"{num1} + {num2} = {add(num1, num2)}")
+print(f"{num1} - {num2} = {subtract(num1, num2)}")
+print(f"{num1} / {num2} = {divide(num1, num2)}")
+print(f"{num1} x {num2} = {multiply(num1, num2)}")
+
+# Greeting function
+def greeting(name):
+    print(f"Hello {name}! Welcome!")
+
+my_name = input("Enter your name: ")
+greeting(my_name)
+
+
+
+
+
+
